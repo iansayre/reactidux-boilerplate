7d721c7a4501f923be8924fdf39d79ea48f4d66c
Ian Sayre <me@iansayre.com>
2018-11-14 10:54:30 -0800

    The basics are all fleshed out. If you wanted to start a react app with out redux, this would work

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>LET'S BUILD A REACT APP</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
